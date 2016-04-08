cccb967de48b788038f44f1a55a7f322f1073224
kjames1988 <kjames@ets.org>
2016-04-07 21:54:47 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -11,4 +11,11 @@ datasub <-data_1 [data_1$Date %in% c("1/2/2007","2/2/2007") ,]
 datetime <- strptime(paste(datasub$Date, datasub$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalAPower <- as.numeric(datasub$Global_active_power)
 
+## plot graph
 plot(datetime,globalAPower,type ="l",main = "Plot 2",xlab= " ",ylab = "Global Active Power(kilowatts)")
+
+##width of 480 pixels and a height of 480 pixels
+dev.copy(png, file = "plot2.png",width = 480, height = 480)
+
+## close the PNG device!
+dev.off()
